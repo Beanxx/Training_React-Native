ac578eb1858707b635d42f71dac7274e258ac462
soobin <tnqls991211@naver.com>
2022-01-04 17:11:11 +0900

    feat: StatusBar 변경

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import DateHead from './components/DateHead';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+
+function App() {
+  const today = new Date();
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView edges={['bottom']}>
+        <DateHead date={today} />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
