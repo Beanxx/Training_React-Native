afdb747b5200ebb010bd60eed9ef42a038a457e6
soobin <tnqls991211@naver.com>
2022-01-06 00:27:54 +0900

    feat: add TodoList component

---
+++ components/TodoList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {FlatList, View, StyleSheet} from 'react-native';
+import TodoItem from './TodoItem';
+
+function TodoList({todos}) {
+  return (
+    <FlatList
+      ItemSeparatorComponent={() => <View style={styles.separator} />}
+      style={styles.list}
+      data={todos}
+      renderItem={({item}) => (
+        <TodoItem id={item.id} text={item.text} done={item.done} />
+      )}
+      keyExtractor={item => item.id.toString()} // 각 항목의 고유값(id) 추출
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  list: {
+    flex: 1,
+  },
+  separator: {
+    backgroundColor: '#e0e0e0',
+    height: 1,
+  },
+});
+
+export default TodoList;
