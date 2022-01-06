ef74987ce81f82aba61f4fdd3e3fdd0df94cf323
soobin <tnqls991211@naver.com>
2022-01-06 17:25:08 +0900

    feat: using vector icon & delete item

--- App.js
+++ App.js
@@ -32,6 +32,11 @@ function App() {
     setTodos(nextTodos);
   };
 
+  const onRemove = id => {
+    const nextTodos = todos.filter(todo => todo.id !== id);
+    setTodos(nextTodos);
+  };
+
   return (
     <SafeAreaProvider>
       <SafeAreaView edges={['bottom']} style={styles.block}>
@@ -42,7 +47,7 @@ function App() {
           {todos.length === 0 ? (
             <Empty />
           ) : (
-            <TodoList todos={todos} onToggle={onToggle} />
+            <TodoList todos={todos} onToggle={onToggle} onRemove={onRemove} />
           )}
           <AddTodo onInsert={onInsert} />
         </KeyboardAvoidingView>
