3a5e9d885912b9e338a8aae15cb58ef642d4c04c
soobin <tnqls991211@naver.com>
2022-01-11 19:16:35 +0900

    feat: preparing WriteScreen UI

--- screens/FeedsScreen.js
+++ screens/FeedsScreen.js
@@ -1,28 +1,17 @@
 import React, {useContext} from 'react';
 import {StyleSheet, View, TextInput} from 'react-native';
-import LogContext from '../contexts/LogContext';
+import FloatingWriteButton from '../components/FloatingWriteButton';
 
 function FeedsScreen() {
-  const {text, setText} = useContext(LogContext);
-
   return (
     <View style={styles.block}>
-      <TextInput
-        value={text}
-        onChangeText={setText}
-        placeholder="텍스트를 입력하세요."
-        style={styles.input}
-      />
+      <FloatingWriteButton />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  block: {},
-  input: {
-    padding: 16,
-    backgroundColor: 'white',
-  },
+  block: {flex: 1},
 });
 
 export default FeedsScreen;
