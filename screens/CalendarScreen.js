7c599ad422264c34e2b0d68fc16fdac46185c155
soobin <tnqls991211@naver.com>
2022-01-16 23:47:29 +0900

    feat: 스크롤 내렸을 때 글쓰기 버튼 숨기기

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,24 +1,42 @@
 import React, {useRef, useState, useEffect} from 'react';
 import {Animated, Button, StyleSheet, View} from 'react-native';
 
-function FadeInAndOut() {
-  const animation = useRef(new Animated.Value(1)).current;
-  const [hidden, setHidden] = useState(false);
+function SlideLeftAndRight() {
+  const animation = useRef(new Animated.Value(0)).current;
+  const [enabled, setEnabled] = useState(false);
 
   useEffect(() => {
     Animated.timing(animation, {
-      toValue: hidden ? 0 : 1,
+      toValue: enabled ? 1 : 0,
       useNativeDriver: true,
     }).start();
-  }, [hidden, animation]);
+  }, [enabled, animation]);
 
   return (
     <View>
-      <Animated.View style={[styles.rectangle, {opacity: animation}]} />
+      <Animated.View
+        style={[
+          styles.rectangle,
+          {
+            transform: [
+              {
+                translateX: animation.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [0, 150],
+                }),
+              },
+            ],
+            opacity: animation.interpolate({
+              inputRange: [0, 1],
+              outputRange: [1, 0],
+            }),
+          },
+        ]}
+      />
       <Button
         title="Toggle"
         onPress={() => {
-          setHidden(!hidden);
+          setEnabled(!enabled);
         }}
       />
     </View>
@@ -28,7 +46,7 @@ function FadeInAndOut() {
 function CalendarScreen() {
   return (
     <View style={styles.block}>
-      <FadeInAndOut />
+      <SlideLeftAndRight />
     </View>
   );
 }
