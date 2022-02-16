f032d17d7a45ca91bbae96f07b9327bf6841d9b7
soobin <tnqls991211@naver.com>
2022-02-16 19:09:01 +0900

    feat: useRecoilCallback을 사용하여 최적화

--- hooks/useTodosAction.ts
+++ hooks/useTodosAction.ts
@@ -1,21 +1,27 @@
 import {useMemo} from 'react';
-import {useRecoilValue, useSetRecoilState} from 'recoil';
+import {useRecoilCallback, useSetRecoilState} from 'recoil';
 import {nextTodoId, todosState} from '../atoms/todos';
 
 export default function useTodosActions() {
   const set = useSetRecoilState(todosState);
-  const nextId = useRecoilValue(nextTodoId);
-
-  return useMemo(
-    () => ({
-      add: (text: string) =>
+  const add = useRecoilCallback(
+    ({snapshot}) =>
+      async (text: string) => {
+        const nextId = await snapshot.getPromise(nextTodoId);
         set(prevState =>
           prevState.concat({
             id: nextId,
             text,
             done: false,
           }),
-        ),
+        );
+      },
+    [set],
+  );
+
+  return useMemo(
+    () => ({
+      add,
       remove: (id: number) =>
         set(prevState => prevState.filter(todo => todo.id !== id)),
       toggle: (id: number) =>
@@ -30,6 +36,6 @@ export default function useTodosActions() {
           ),
         ),
     }),
-    [set, nextId],
+    [add, set],
   );
 }
